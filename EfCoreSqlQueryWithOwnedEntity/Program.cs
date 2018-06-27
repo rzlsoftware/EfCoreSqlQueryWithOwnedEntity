5a905f6b9226dddfa485d28ae58fc5b8376d372f
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-06-27 17:10:55 +0200

    Add project files.

---
+++ EfCoreSqlQueryWithOwnedEntity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCoreSqlQueryWithOwnedEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
